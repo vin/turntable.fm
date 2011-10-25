d583524b61fbfd2b386a215a24096c8c14e654ec
Vineet Kumar <vineet@doorstop.net>
2011-10-24 23:35:17 -0400

    Add some basic handlers.
    The basic bot framework is in place, and it's useful interactively.
    Start adding some basic handlers that respond to users entering
    and giving text commands.

--- bots/bot.js
+++ bots/bot.js
@@ -10,6 +10,8 @@ Bot = function(configFile) {
 	this.configFile = configFile || process.argv[2] || Bot.usage();
 	this.config = {};
 	this.greetings = {};
+	this.logChats = false;
+	this.speechHandlers = {};
 };
 
 Bot.usage = function() {
@@ -21,10 +23,21 @@ Bot.prototype.start = function() {
 	fs.readFile(self.configFile, 'utf8', function(err, data) {
 		if (err) throw err;
 		self.config = JSON.parse(data);
-		repl.start(self.config.name + "> ").context.bot = self;
+		var prompt = path.basename(self.configFile, path.extname(self.configFile));
+		var replContext = repl.start(prompt + "> ").context
+		replContext.bot = self;
 		self.readGreetings();
 		self.ttapi = new ttapi(self.config.auth, self.config.userid, self.config.roomid);
+		replContext.say = self.ttapi.speak.bind(self.ttapi);
+		self.bindHandlers();
 	});
+};
+
+Bot.prototype.bindHandlers = function() {
+	this.ttapi.on('speak', this.onSpeak.bind(this));
+	this.ttapi.on('registered', this.onRegistered.bind(this));
+	this.speechHandlers['help'] = this.onHelp.bind(this);
+	this.speechHandlers['commands'] = this.onHelpCommands.bind(this);
 };
 
 Bot.prototype.readGreetings = function() {
@@ -36,5 +49,60 @@ Bot.prototype.readGreetings = function() {
 		console.log('loaded %d greetings', Object.keys(self.greetings).length);
 	});
 };
+
+/**
+  * @param {{name: string, text: string}} data return by ttapi
+  */
+Bot.prototype.onSpeak = function(data) {
+	if (this.debug) {
+		console.dir(data);
+	}
+	if (this.logChats) {
+		console.log('chat: %s: %s', data.name, data.text);
+	}
+        var words = data.text.split(/\s+/);
+        var command = words[0].toLowerCase();
+        if (command.match(/^[!*\/]/)) {
+                command = command.substring(1);
+        } else if (Bot.bareCommands.indexOf(data.text) == -1) { // bare commands must match the entire text line
+                return;
+        }
+        var handler = this.speechHandlers[command];
+        if (handler) {
+                handler(data.text, data.name);
+        }
+};
+
+Bot.prototype.onHelp = function() {
+	this.ttapi.speak(this.config.messages.help.replace("{room.name}", this.roomInfo.room.name));
+};
+
+Bot.prototype.onHelpCommands = function() {
+	this.ttapi.speak(
+		'commands: ' + Object.keys(this.speechHandlers).map(function(s) { return "*" + s; })).join(', ');
+};
+
+Bot.prototype.onRegistered = function(data) {
+	if (this.debug) {
+		console.dir(data);
+	}
+	user = data.user[0];
+	if (user.userid == this.config.userid) {
+		this.ttapi.roomInfo(this.onRoomInfo.bind(this));
+	} else {
+		this.ttapi.speak(this.config.messages.defaultGreeting.replace("{user.name}", user.name));
+	}
+};
+
+Bot.prototype.onRoomInfo = function(data) {
+	if (this.debug) {
+		console.dir(data);
+	}
+	this.roomInfo = data;
+};
+
+Bot.bareCommands = [
+	'help',
+];
 
 exports.Bot = Bot;
---
+++ bots/example.js
@@ -0,0 +1,4 @@
+var Bot = require('bots');
+
+var bot = new Bot('example.json');
+bot.start();
